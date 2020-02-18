987bfe2da4e5c05dec9e9b744127e3a44edafff9
Mark Kenny <beingmrkenny@gmail.com>
2020-02-18 01:06:37 +0000

    Edit page always goes to desktop

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-var url = window.location.href;
-if (/https?:\/\/[a-z][a-z]\.(:?wikipedia|wiktionary)\.org/.test(url)) {
-	var newUrl = url.replace(/(https?:\/\/)([a-z][a-z])\.(wikipedia|wiktionary)\.org/, "$1$2.m.$3.org");
-	window.location.replace(newUrl);
+handleRedirect();
+window.onhashchange = handleRedirect;
+
+function handleRedirect () {
+	var url = window.location;
+
+	if (/https?:\/\/[a-z][a-z]\.(:?wikipedia|wiktionary)\.org/.test(url.href)) {
+		if (!url.search.includes('action=edit')) {
+			// turn: https://en.wikipedia.org/wiki/Neferefre
+			// into: https://en.m.wikipedia.org/wiki/Neferefre
+			var newUrl = url.href.replace(/(https?:\/\/)([a-z][a-z])\.(wikipedia|wiktionary)\.org/, "$1$2.m.$3.org");
+			window.location.replace(newUrl);
+		}
+	}
+
+	if (/https?:\/\/[a-z][a-z]\.m\.(:?wikipedia|wiktionary)\.org/.test(url.href)) {
+		if (url.hash.includes('#/editor/')) {
+			// turn: https://en.m.wikipedia.org/wiki/Neferefre#/editor/0
+			// and:  https://en.m.wikipedia.org/w/index.php?title=Neferefre#/editor/all
+			// into: https://en.wikipedia.org/w/index.php?title=Neferefre&action=edit
+			let title = (url.pathname == '/w/index.php')
+				? url.search.replace(/.*title=([^&]+).*/, "$1")
+				: url.pathname.replace(/\/w(iki)\//, '');
+			let newUrl =
+				url.protocol + '//' +
+				url.hostname.replace(/([a-z][a-z])\.m\.(wikipedia|wiktionary)\.org/, "$1.$2.org") +
+				'/w/index.php?' +
+				'title=' + title +
+				'&action=edit';
+			window.location.replace(newUrl);
+		}
+	}
 }
