475ec6eda013ebff3e29013ddf266c685190bfcb
Mark Kenny <beingmrkenny@gmail.com>
2020-02-25 00:17:33 +0000

    Don't redirect "www" domains, they don't exist on mobile

--- script.js
+++ script.js
@@ -10,42 +10,42 @@ function handleRedirect () {
 
 	let wikimediaHosts = 'wikibooks|wikidata|wikimedia|wikinews|wikipedia|wikiquote|wikisource|wikiversity|wikivoyage|wiktionary';
 
-	let testDesktopURLRegex = new RegExp(`https?:\/\/[a-z]+\.(:?${wikimediaHosts})\.org`),
-		matchDesktopURLRegex = new RegExp(`([a-z]+)\.(${wikimediaHosts})\.org`),
-		testMobileURLRegex = new RegExp(`https?:\/\/[a-z]+\.m\.(:?${wikimediaHosts})\.org`),
-		matchMobileURLRegex = new RegExp(`([a-z]+)\.m\.(${wikimediaHosts})\.org`);
+	let desktopURLRegex = new RegExp(`^https?:\/\/([a-z]+)\.(${wikimediaHosts})\.org`),
+		mobileURLRegex = new RegExp(`^https?:\/\/([a-z]+)\.m\.(:?${wikimediaHosts})\.org`);
 
 	chrome.storage.local.get('redirect-edit-page', results => {
 
 		let redirectEditPage = (results['redirect-edit-page'] == false) ? false : true;
+		let desktopMatches = url.href.match(desktopURLRegex);
 
-		if (testDesktopURLRegex.test(url.href)) {
+		if (desktopMatches && desktopMatches[1] !== 'www') {
 			if (
 				(redirectEditPage == true && !url.search.includes('action=edit'))
 				|| redirectEditPage == false
 			) {
 				// turn: https://en.wikipedia.org/wiki/Neferefre
 				// into: https://en.m.wikipedia.org/wiki/Neferefre
-				var newURL = url.href.replace(matchDesktopURLRegex, "$1.m.$2.org");
+				var newURL = url.href.replace(desktopURLRegex, "https://$1.m.$2.org");
 				window.location.replace(newURL);
 			}
 		}
 
-		if (testMobileURLRegex.test(url.href)) {
+		if (mobileURLRegex.test(url.href)) {
 			if (redirectEditPage == true && url.hash.includes('#/editor/')) {
 				// turn: https://en.m.wikipedia.org/wiki/Neferefre#/editor/0
-				// and:  https://en.m.wikipedia.org/w/index.php?title=Neferefre#/editor/all
+				// or:   https://en.m.wikipedia.org/w/index.php?title=Neferefre#/editor/all
 				// into: https://en.wikipedia.org/w/index.php?title=Neferefre&action=edit
 				let title = (url.pathname == '/w/index.php')
 					? url.search.replace(/.*title=([^&]+).*/, "$1")
 					: url.pathname.replace(/\/w(iki)\//, '');
-				let newURL =
+
+				window.location.replace(
 					url.protocol + '//' +
-					url.hostname.replace(matchMobileURLRegex, "$1.$2.org") +
+					url.hostname.replace(mobileURLRegex, "$1.$2.org") +
 					'/w/index.php?' +
 					'title=' + title +
-					'&action=edit';
-				window.location.replace(newURL);
+					'&action=edit'
+				);
 			}
 		}
 
