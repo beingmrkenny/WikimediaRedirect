64dd371505a83dc96edbeb8a5f916a7e10cede72
Mark Kenny <beingmrkenny@gmail.com>
2020-05-02 02:13:50 +0100

    Gulp file updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const EXTENSION_NAME = 'WikimediaRedirect';
+
 const { src, dest, series, parallel, watch } = require('gulp');
 // const rename = require('gulp-rename');
 
@@ -7,8 +9,8 @@ function releaseZip () {
 	const fs = require('fs-extra');
 	const zip = require('gulp-zip');
 
-	fs.emptyDirSync('/tmp/WikimediaRedirect');
-	try { fs.removeSync(process.env.HOME+'/Desktop/WikimediaRedirect.zip') } catch (err) { console.log(err); }
+	fs.emptyDirSync(`/tmp/${EXTENSION_NAME}`);
+	try { fs.removeSync(`${process.env.HOME}/Desktop/${EXTENSION_NAME}.zip`) } catch (err) { console.log(err); }
 
 	let includes = [
 		'img',
@@ -22,15 +24,15 @@ function releaseZip () {
 	];
 
 	for (let entry of includes) {
-		fs.copySync('./'+entry, '/tmp/WikimediaRedirect/'+entry);
+		fs.copySync('./'+entry, `/tmp/${EXTENSION_NAME}/`+entry);
 	}
 
 	console.log('Run these commands to check your zip for lice');
-	console.log('zip -d ~/Desktop/WikimediaRedirect.zip __MACOSX/\*');
-	console.log('unzip -vl ~/Desktop/WikimediaRedirect.zip');
+	console.log(`zip -d ~/Desktop/${EXTENSION_NAME}.zip __MACOSX/\*`);
+	console.log(`unzip -vl ~/Desktop/${EXTENSION_NAME}.zip`);
 
-	return src('/tmp/WikimediaRedirect/**/*')
-		.pipe(zip('WikimediaRedirect.zip'))
+	return src(`/tmp/${EXTENSION_NAME}/**/*`)
+		.pipe(zip(`${EXTENSION_NAME}.zip`))
 		.pipe(dest(process.env.HOME+'/Desktop'));
 }
 
